846574b787e46d663b9ef063151bbd3e7f5477f4
Jhagas Winaya <jhagashana@mailbox.org>
2021-12-30 01:49:41 +0700

    React code Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+
+const remaining = () => {
+  const tahunbaru = "Jan 01 2022";
+  let difference = new Date(tahunbaru) - new Date();
+
+  let timeLeft = false;
+
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  }
+
+  timeLeft = false;
+  return timeLeft;
+};
+
+function Countdown() {
+  const [timeLeft, setTimeLeft] = React.useState(remaining());
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(remaining());
+    }, 1000);
+    return () => clearTimeout(timer);
+  });
+
+  return (
+    <>
+      {timeLeft ? (
+    <div className="All">
+        <div
+          className="flex-container"
+          style={{
+            display: "flex",
+          }}
+        >
+          <div className="box days">
+            <h1>{timeLeft.days}</h1>
+            <h3>hari</h3>
+          </div>
+          <span>:</span>
+          <div className="box hours">
+            <h1>{timeLeft.hours}</h1>
+            <h3>jam</h3>
+          </div>
+          <span>:</span>
+          <div className="box minutes">
+            <h1>{timeLeft.minutes}</h1>
+            <h3>menit</h3>
+          </div>
+          <span>:</span>
+          <div className="box seconds">
+            <h1>{timeLeft.seconds}</h1>
+            <h3>detik</h3>
+          </div>
+        </div>
+    </div>
+      ) : (
+        <div className="bg">
+          <h1>UBAH SEKAREPMU RUD..</h1>
+        </div>
+      )}
+    </>
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Countdown />;
 }
 
 export default App;
